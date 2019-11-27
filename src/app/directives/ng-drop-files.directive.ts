62d1b7e6baf101db144528ddd5876f3a37a38c5c
wilmer142 <wilmercollege@gmail.com>
2019-11-26 19:49:56 -0500

    Implementing validations on directive

--- src/app/directives/ng-drop-files.directive.ts
+++ src/app/directives/ng-drop-files.directive.ts
@@ -1,10 +1,12 @@
 import { Directive, EventEmitter, ElementRef, HostListener, Input, Output } from '@angular/core';
+import { FileItem } from '../models/file-item';
 
 @Directive({
   selector: '[appNgDropFiles]'
 })
 export class NgDropFilesDirective {
   
+  @Input() archivos: FileItem[] = [];
   @Output() mouseSobre: EventEmitter<boolean> = new EventEmitter();
 
   constructor() { }
@@ -17,6 +19,32 @@ export class NgDropFilesDirective {
   @HostListener('dragleave', ['$event'])
   public onDragLeave( event: any ) {
     this.mouseSobre.emit(false);
+  }
+
+  //Validations
+
+  private _archivoPuedeSerCargado( archivo: File ): boolean {
+    return (!this._archivoYaFueDropeado(archivo.name) && this._esImagen(archivo.type));
+  }
+
+  private _prevenirDetener(event) {
+    event.preventDefault();
+    event.stopPropagation();
+  }
+
+  private _archivoYaFueDropeado(nombreArchivo:string): boolean {
+    for( const archivo of this.archivos) {
+      if ( archivo.nombreArchivo == nombreArchivo) {
+        console.log(`El archivo ${nombreArchivo} ya está agregado`);
+        return true;
+      }
+    }
+
+    return false;
+  }
+
+  private _esImagen( tipoArchivo: string) {
+    return ( tipoArchivo === '' || tipoArchivo === undefined ) ? false : tipoArchivo.startsWith('image');
   }
 
 }
