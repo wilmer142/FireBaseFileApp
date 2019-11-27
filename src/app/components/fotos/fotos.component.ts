1c540953be015c44f705c27cde80a9596b74876c
wilmer142 <wilmercollege@gmail.com>
2019-11-26 20:57:30 -0500

    Implementing fotos component

--- src/app/components/fotos/fotos.component.ts
+++ src/app/components/fotos/fotos.component.ts
@@ -1,5 +1,11 @@
 import { Component, OnInit } from '@angular/core';
 
+import { AngularFirestore, AngularFirestoreCollection } from '@angular/fire/firestore';
+import { Observable } from 'rxjs';
+import { FileItem } from '../../models/file-item';
+
+export interface Item { name: string; }
+
 @Component({
   selector: 'app-fotos',
   templateUrl: './fotos.component.html',
@@ -7,7 +13,13 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FotosComponent implements OnInit {
 
-  constructor() { }
+  private itemsCollection: AngularFirestoreCollection<FileItem>;
+  items: Observable<FileItem[]>;
+
+  constructor(private afs: AngularFirestore) {
+    this.itemsCollection = afs.collection<FileItem>('img');
+    this.items = this.itemsCollection.valueChanges();
+  }
 
   ngOnInit() {
   }
